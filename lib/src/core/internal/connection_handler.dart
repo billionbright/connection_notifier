3fbc277ab2f986e99189d137e554b7582aaabd81
bbright-viral <149167233+bbright-viral@users.noreply.github.com>
2025-08-12 12:52:46 +1200

    update dependency

--- lib/src/core/internal/connection_handler.dart
+++ lib/src/core/internal/connection_handler.dart
@@ -3,23 +3,21 @@ import 'package:internet_connection_checker/internet_connection_checker.dart'
     show InternetConnectionChecker, InternetConnectionStatus;
 import 'package:rxdart/rxdart.dart';
 
-import 'connection_notifier_internet_connection_status.dart'
-    show ConnectionNotifierInternetConnectionStatus;
+import 'connection_notifier_internet_connection_status.dart' show ConnectionNotifierInternetConnectionStatus;
 
 @immutable
 class ConnectionHandler {
-  static Stream<ConnectionNotifierInternetConnectionStatus>
-      get onStatusChange =>
-          InternetConnectionChecker().onStatusChange.switchMap(
-            (value) {
-              switch (value) {
-                case InternetConnectionStatus.connected:
-                  return Stream.value(
-                      ConnectionNotifierInternetConnectionStatus.connected);
-                case InternetConnectionStatus.disconnected:
-                  return Stream.value(
-                      ConnectionNotifierInternetConnectionStatus.disconnected);
-              }
-            },
-          );
+  static Stream<ConnectionNotifierInternetConnectionStatus> get onStatusChange =>
+      InternetConnectionChecker.instance.onStatusChange.switchMap(
+        (value) {
+          switch (value) {
+            case InternetConnectionStatus.connected:
+              return Stream.value(ConnectionNotifierInternetConnectionStatus.connected);
+            case InternetConnectionStatus.disconnected:
+              return Stream.value(ConnectionNotifierInternetConnectionStatus.disconnected);
+            case InternetConnectionStatus.slow:
+             return Stream.value(ConnectionNotifierInternetConnectionStatus.connected);
+          }
+        },
+      );
 }
